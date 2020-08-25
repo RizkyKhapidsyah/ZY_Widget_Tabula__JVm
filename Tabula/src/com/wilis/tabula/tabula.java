f7f374053fd23b1d1cbd10d4503ae49131b398f0
RizkyKhapidsyah <RizkyKhapidsyah@RIKY_PC>
2020-08-25 10:59:21 +0700

    Initial commit

---
+++ Tabula/src/com/wilis/tabula/tabula.java
@@ -0,0 +1,13 @@
+package com.wilis.tabula;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class tabula extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
